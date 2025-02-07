5396b5e9cb020edc8819eb80b788a9f160a07ab1
Daniel Kirsch <danishkirel@gmail.com>
2025-02-07 13:29:04 +0100

    Add secrets

--- secrets.sh
+++ secrets.sh
@@ -1,3 +1,5 @@
 export HASS_TOKEN="{{ op://Private/Home Assistant/access_token }}"
 export OPENAI_API_KEY="{{ op://Private/OpenAI API Key/Anmeldedaten }}"
-export CLAUDE_API_KEY="{{ op://Private/Claude/Anmeldedaten }}"
+export ANTHROPIC_API_KEY="{{ op://Private/Claude/Anmeldedaten }}"
+export OPENROUTER_API_KEY="{{ op://Private/Openrouter/Anmeldedaten }}"
+export DEEPSEEK_API_KEY="{{ op://Private/cvtvyubg6wtxjhe3pjwb7z5w6e/Anmeldedaten }}"
