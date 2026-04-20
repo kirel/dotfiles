b0661dac6139677e1205666d52f0509be34f5d56
Daniel Kirsch <daniel.kirsch@allunity.com>
2026-04-20 11:20:10 +0200

    chore(secrets): refresh work and personal credential templates

--- .secrets.work.sh
+++ .secrets.work.sh
@@ -2,3 +2,4 @@
 export GITHUB_PERSONAL_ACCESS_TOKEN={{op://Employee/Github PAT/Anmeldedaten}}
 export GITHUB_MCP_PAT=$GITHUB_PERSONAL_ACCESS_TOKEN
 export GH_TOKEN=$GITHUB_PERSONAL_ACCESS_TOKEN
+export DUNE_API_KEY={{op://Employee/Dune/API Key}}
